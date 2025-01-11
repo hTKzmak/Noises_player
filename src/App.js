4b96d25908ed4eff3ed852c90b43ec1de446986f
hTKzmak <ineedrtx@gmail.com>
2025-01-11 18:02:00 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -146,7 +146,7 @@ const App = () => {
         currentSong={currentSong}
 
         setShowPlayer={setShowPlayer}
-        
+
         showMobilePlayer={showMobilePlayer}
         setShowMobilePlayer={setShowMobilePlayer}
       />
